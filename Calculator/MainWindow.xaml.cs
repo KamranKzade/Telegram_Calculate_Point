4fe3cc626d33f5529f78f84dfaa517af6750aa0b
Kamran <311.kamran@gmail.com>
2022-11-14 02:02:50 +0400

    Add Calculator design

---
+++ Calculator/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace Calculator;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void Button_Click(object sender, RoutedEventArgs e) => MessageBox.Show("will be generated in the future", "Information", MessageBoxButton.OK, MessageBoxImage.Information);
+
+
+}
